d759a78453e4c3e6380dff1755dd1c395baeefee
Tanzim Ahmed <mr.tanzim.ahmed@gmail.com>
2025-05-11 16:28:34 +0600

    modification test

--- src/Assignment01/HelloWorld.java
+++ src/Assignment01/HelloWorld.java
@@ -1,8 +1,8 @@
 package Assignment01;
 
-public class HelloWorld {
-    public static void main(String[] args) {
+public class HelloWorld { public static void main(String[] args) {
         System.out.println("Hello, World!");
+	System.out.print("Hello!");
 
     }
 }
